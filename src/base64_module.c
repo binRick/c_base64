a4444b7c3cc465244eceafcda1034f097b0c05cf
Richard Blundell <none@none.com>
2022-03-30 21:00:52 +0000

    m

--- src/base64_module.c
+++ src/base64_module.c
@@ -13,42 +13,7 @@
 /*******************/
 
 /*******************/
-module(base64_module) * cm_base64;
+#include "../include/globals.h"
+#include "../src/globals.c"
 /*******************/
 
-
-/*******************/
-void pre(void) {
-  log_set_level(MODULE_LOG_LEVEL);
-  cm_base64 = require(base64_module);
-}
-
-
-/*******************/
-void test(void) {
-}
-
-
-/*******************/
-
-
-/*******************/
-void exec(void){
-  log_info("base64 exec>");
-}
-
-
-/*******************/
-
-
-/*******************/
-void post(void) {
-  log_debug("base64 unload>");
-  clib_module_free(cm_base64);
-}
-
-
-/*******************/
-
-
-/*******************/
