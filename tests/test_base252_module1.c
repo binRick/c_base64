f1c9d09933663972fab6846c615a00d29e7e127a
Richard Blundell <none@none.com>
2022-04-21 16:36:38 +0000

    integrate base252

--- tests/test_base252_module1.c
+++ tests/test_base252_module1.c
@@ -19,24 +19,13 @@ void b252_test(){
   char *orig = "THE SOFTWARE IS PROVIDED \"AS IS\" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.";
 
   strcpy(input, orig);
-
   strcpy(output, "");
-
   size = data_to_base252(input, strlen(input), output, BUFFER_SIZE);
-
-  printf("input is:\n\n%s\n(%d) \n\noutput is:\n\n%s\n\n", input, strlen(input), output);
-
   memcpy(input, output, size + 1);
-
-  strcpy(output, "");
-  LOGGER("encoded %db string '%s' => %db string '%s'",
-         strlen(input), input, strlen(output), output);
-
   base252_to_data(input, size, output, BUFFER_SIZE);
 
-  printf("input is:\n\n%s\n(%d)\n\noutput is:\n\n%s\n\n", input, size, output);
-  LOGGER("decoded %db string '%s' => %db string '%s'",
-         strlen(input), input, strlen(output), output);
+  LOGGER("decoded %db string => %db string '%s'",
+         strlen(input), strlen(output), output);
 
   assert(strcmp(output, orig) == 0);
 }
